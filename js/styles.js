d6d58c2d43b24cbe5faabffe758d73f9fadabbea
Jose P. Núñez <thyrion1976@gmail.com>
2023-12-09 14:18:00 +0100

    first commit

---
+++ js/styles.js
@@ -0,0 +1,32 @@
+//(function($) {
+    "use strict";
+
+    // manual carousel controls
+   // $('.next').click(function(){ $('.carousel').carousel('next');return false; });
+   // $('.prev').click(function(){ $('.carousel').carousel('prev');return false; });
+    
+//})(jQuery);
+
+/*agrandar imagen on click*/
+
+$('img[data-enlargeable]').addClass('img-enlargeable').click(function(){
+    var src = $(this).attr('src');
+    var modal;
+    function removeModal(){ modal.remove(); $('body').off('keyup.modal-close'); }
+    modal = $('<div>').css({
+        background: 'RGBA(0,0,0,.5) url('+src+') no-repeat center',
+        backgroundSize: 'contain',
+        width:'100%', height:'100%',
+        position:'fixed',
+        zIndex:'10000',
+        top:'0', left:'0',
+        cursor: 'zoom-out'
+    }).click(function(){
+        removeModal();
+    }).appendTo('body');
+    //handling ESC
+    $('body').on('keyup.modal-close', function(e){
+      if(e.key==='Escape'){ removeModal(); } 
+    });
+});
+
